449cd2c95b892966c9d6a23571804b38619ae75f
Elyasaf Elbaz <47217505+elyasaf755@users.noreply.github.com>
2021-02-22 22:19:45 +0200

    Meh

--- EcoBuy/ViewModels/MainWindowVM.cs
+++ EcoBuy/ViewModels/MainWindowVM.cs
@@ -1,13 +1,13 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using EcoBuy.Models;
 
 namespace EcoBuy.ViewModels
 {
     public class MainWindowVM : ViewModelBase
     {
 
+        public MainWindowVM()
+        {
+            
+        }
     }
 }
