524641b8da224370fdd6d272a50df5b263d3c0d9
Elyasaf Elbaz <47217505+elyasaf755@users.noreply.github.com>
2021-02-22 19:17:58 +0200

    PieChartUC data binding works

---
+++ EcoBuy/Models/CategoiesCount.cs
@@ -0,0 +1,11 @@
+﻿
+namespace EcoBuy.Models
+{
+    public class CategoiesCount
+    {
+        public int Electricity { get; set; } = 10;
+        public int Fashion { get; set; } = 4;
+        public int Food { get; set; } = 20;
+        public int Health { get; set; } = 5;
+    }
+}
---
+++ EcoBuy/UserControls/PieChart/PieChartUC.xaml.cs
@@ -0,0 +1,72 @@
+﻿using EcoBuy.Models;
+using LiveCharts;
+using LiveCharts.Wpf;
+using System;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace EcoBuy.UserControls
+{
+    /// <summary>
+    /// Interaction logic for PieChartUC.xaml
+    /// </summary>
+    public partial class PieChartUC : UserControl
+    {
+        public static readonly DependencyProperty CategoryPieChartModelProperty = DependencyProperty.Register(
+                                                                               "CategoryPieChartModel",
+                                                                               typeof(PieChartDataModel),
+                                                                               typeof(PieChartUC),
+                                                                               new PropertyMetadata(default(PieChartDataModel)));
+        public PieChartDataModel CategoryPieChartModel
+        {
+            get => (PieChartDataModel)GetValue(PieChartUC.CategoryPieChartModelProperty);
+            set => SetValue(PieChartUC.CategoryPieChartModelProperty, value);
+        }
+
+        public CategoiesCount CategoiesCount = new CategoiesCount();
+
+        public PieChartUC()
+        {
+            InitializeComponent();
+
+            DataContext = this;
+
+            PointLabel = chartPoint =>
+                string.Format("{0} ({1:P})", chartPoint.Y, chartPoint.Participation);
+            
+            CreatePieChartData(CategoiesCount);
+
+            Task.Delay(2000).ContinueWith(t =>
+            {
+                Dispatcher.Invoke(() =>
+                {
+                    CategoiesCount = new CategoiesCount() { Electricity = 10, Fashion = 5, Food = 5, Health = 20 };
+                    CreatePieChartData(CategoiesCount);
+                });
+                
+            });
+
+            
+        }
+
+        public Func<ChartPoint, string> PointLabel { get; set; }
+
+        private void CreatePieChartData(CategoiesCount categoriesCount)
+        {
+            this.CategoryPieChartModel = new PieChartDataModel(categoriesCount);
+        }
+
+        private void Chart_OnDataClick(object sender, ChartPoint chartpoint)
+        {
+            var chart = (LiveCharts.Wpf.PieChart)chartpoint.ChartView;
+
+            //clear selected slice.
+            foreach (PieSeries series in chart.Series)
+                series.PushOut = 0;
+
+            var selectedSeries = (PieSeries)chartpoint.SeriesView;
+            selectedSeries.PushOut = 8;
+        }
+    }
+}
