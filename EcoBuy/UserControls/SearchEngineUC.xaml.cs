b33ed6018ab5ae01965c0bd4fe5760a5ccb22149
Elyasaf Elbaz <47217505+elyasaf755@users.noreply.github.com>
2021-02-23 18:26:59 +0200

    renamed UI to UC

--- EcoBuy/UserControls/SearchEngineUC.xaml.cs
+++ EcoBuy/UserControls/SearchEngineUC.xaml.cs
@@ -4,11 +4,11 @@ using System.Windows.Controls;
 namespace EcoBuy.UserControls
 {
     /// <summary>
-    /// Interaction logic for SearchEngineUI.xaml
+    /// Interaction logic for SearchEngineUC.xaml
     /// </summary>
-    public partial class SearchEngineUI : UserControl
+    public partial class SearchEngineUC : UserControl
     {
-        public SearchEngineUI()
+        public SearchEngineUC()
         {
             InitializeComponent();
             this.DataContext = new SearchEngineUCVM();
