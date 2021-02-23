e25ab236ef3666a99816a577a0473745ee6fb804
Evyatar Revivo <50528768+erevivo@users.noreply.github.com>
2021-02-23 11:45:57 +0200

    GoogleApi

--- EcoBuy/MainWindow.xaml.cs
+++ EcoBuy/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using PL.ConnectToGoogleDrive;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -20,11 +21,13 @@ namespace EcoBuy
     /// </summary>
     public partial class MainWindow : Window
     {
+        private GoogleDriveApi downloadQr;
         public MainWindow()
         {
             InitializeComponent();
+            downloadQr = new GoogleDriveApi();
+            downloadQr.Connect();
         }
-
         private void Button_Click(object sender, RoutedEventArgs e)
         {
             this.Close();
