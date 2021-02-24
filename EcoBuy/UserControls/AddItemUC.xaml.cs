2fde95668301925d8e710b7cc6ebe0f11d61a8c9
Evyatar Revivo <50528768+erevivo@users.noreply.github.com>
2021-02-24 19:56:53 +0200

    Update AddItemUC.xaml.cs

--- EcoBuy/UserControls/AddItemUC.xaml.cs
+++ EcoBuy/UserControls/AddItemUC.xaml.cs
@@ -26,6 +26,7 @@ namespace EcoBuy.UserControls
         public AddItemUC()
         {
             InitializeComponent();
+            var d = new AddItemUCVM();
             this.DataContext = new AddItemUCVM();
         }
         private void IcecreamDropZone_DragOver(object sender, DragEventArgs e)
