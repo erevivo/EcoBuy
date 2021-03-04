d556e8bdf3ccec3c8fd2d98aff0142d60c3e5693
Evyatar Revivo <50528768+erevivo@users.noreply.github.com>
2021-03-04 12:31:10 +0200

    meh

--- EcoBuy/ViewModels/UserControls/DesignModels/SearchEngineUCDM.cs
+++ EcoBuy/ViewModels/UserControls/DesignModels/SearchEngineUCDM.cs
@@ -32,7 +32,7 @@ namespace EcoBuy.ViewModels
                 new PurchasedProduct(new DateTime(2020, 10, 2), 10, 1, 1, "פתיתים אפויים 500 גר' אסם", "C:\\Users\\Evyatar\\Desktop\\2.jpg", ProductsCategory.Food),
                 new PurchasedProduct(new DateTime(2020, 11, 2), 5, 2, 2, "לחם אחיד פרוס אנג'ל", "C:\\Users\\Evyatar\\Desktop\\1.jpg", ProductsCategory.Food),
                 new PurchasedProduct(new DateTime(2020, 12, 2), 20, 3, 3, "חלב 3% שומן תנובה", "C:\\Users\\Evyatar\\Desktop\\3.jpg", ProductsCategory.Food),
-                new PurchasedProduct(new DateTime(2020, 12, 2), 7, 4, 4, "Acamol", null, ProductsCategory.Health)
+                new PurchasedProduct(new DateTime(2020, 12, 2), 7, 4, 4, "מחשב אינטל NUC", "https://www.ivory.co.il/files/catalog/org/1566199487C87HL.jpg", ProductsCategory.Electricity)
             };
         }
     }
--- EcoBuy/ViewModels/UserControls/PieChartUCVM.cs
+++ EcoBuy/ViewModels/UserControls/PieChartUCVM.cs
@@ -1,6 +1,8 @@
-﻿using EcoBuy.Models;
+﻿using EcoBuy.BE;
+using EcoBuy.Models;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +12,7 @@ namespace EcoBuy.ViewModels
     public class PieChartUCVM : ViewModelBase
     {
         public CategoriesCount CategoriesCount { get; set; }
+        public ObservableCollection<PurchasedProduct> PurchasedProducts { get; private set; }
 
         public PieChartUCVM()
         {
@@ -19,6 +22,13 @@ namespace EcoBuy.ViewModels
         private void Test()
         {
             CategoriesCount = new CategoriesCount() { Electricity = 30, Fashion = 5, Food = 5, Health = 20 };
+            PurchasedProducts = new ObservableCollection<PurchasedProduct>()
+            {
+                new PurchasedProduct(new DateTime(2020, 10, 2), 10, 1, 1, "פתיתים אפויים 500 גר' אסם", "C:\\Users\\Evyatar\\Desktop\\2.jpg", ProductsCategory.Food),
+                new PurchasedProduct(new DateTime(2020, 11, 2), 5, 2, 2, "לחם אחיד פרוס אנג'ל", "C:\\Users\\Evyatar\\Desktop\\1.jpg", ProductsCategory.Food),
+                new PurchasedProduct(new DateTime(2020, 12, 2), 20, 3, 3, "חלב 3% שומן תנובה", "C:\\Users\\Evyatar\\Desktop\\3.jpg", ProductsCategory.Food),
+                new PurchasedProduct(new DateTime(2020, 12, 2), 7, 4, 4, "מחשב אינטל NUC", "https://www.ivory.co.il/files/catalog/org/1566199487C87HL.jpg", ProductsCategory.Electricity)
+            };
         }
     }
 }
