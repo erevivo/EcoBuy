2ce019f900354b7faa9bd07051b0177e2acd0a38
Evyatar Revivo <50528768+erevivo@users.noreply.github.com>
2021-03-04 11:20:06 +0200

    finish-SearchUnit

--- EcoBuy/ViewModels/UserControls/SearchEngineUCVM.cs
+++ EcoBuy/ViewModels/UserControls/SearchEngineUCVM.cs
@@ -69,9 +69,9 @@ namespace EcoBuy.ViewModels
         {
             PurchasedProducts = new ObservableCollection<PurchasedProduct>()
             {
-                new PurchasedProduct(new DateTime(2020, 10, 2), 10, 1, 1, "פתיתים", "C:\\Users\\Evyatar\\Desktop\\2.jpg", ProductsCategory.Food),
-                new PurchasedProduct(new DateTime(2020, 11, 2), 5, 2, 2, "Bread", null, ProductsCategory.Food),
-                new PurchasedProduct(new DateTime(2020, 12, 2), 20, 3, 3, "T-Shirt", null, ProductsCategory.Fashion),
+                new PurchasedProduct(new DateTime(2020, 10, 2), 10, 1, 1, "פתיתים אפויים 500 גר' אסם", "C:\\Users\\Evyatar\\Desktop\\2.jpg", ProductsCategory.Food),
+                new PurchasedProduct(new DateTime(2020, 11, 2), 5, 2, 2, "לחם אחיד פרוס אנג'ל", "C:\\Users\\Evyatar\\Desktop\\1.jpg", ProductsCategory.Food),
+                new PurchasedProduct(new DateTime(2020, 12, 2), 20, 3, 3, "חלב 3% שומן תנובה", "C:\\Users\\Evyatar\\Desktop\\3.jpg", ProductsCategory.Food),
                 new PurchasedProduct(new DateTime(2020, 12, 2), 7, 4, 4, "Acamol", null, ProductsCategory.Health)
             };
 
