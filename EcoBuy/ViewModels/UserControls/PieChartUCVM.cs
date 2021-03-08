7640e2c5b95e177bb4239ed7339722914002f94d
Evyatar Revivo <50528768+erevivo@users.noreply.github.com>
2021-03-08 19:11:51 +0200

    UpdateChanges

--- EcoBuy/ViewModels/UserControls/PieChartUCVM.cs
+++ EcoBuy/ViewModels/UserControls/PieChartUCVM.cs
@@ -7,6 +7,7 @@ using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Data;
 using System.Windows.Input;
 
 namespace EcoBuy.ViewModels
@@ -29,15 +30,16 @@ namespace EcoBuy.ViewModels
             CategoriesCount = new CategoriesCount();
             PurchasedProducts = new ObservableCollection<PurchasedProduct>()
             {
-                new PurchasedProduct(new DateTime(2020, 3, 5), 10, 1, 1, "פתיתים אפויים 500 גר' אסם", "C:\\Users\\Evyatar\\Desktop\\2.jpg", ProductsCategory.Food),
+                new PurchasedProduct(new DateTime(2021, 3, 8), 10, 1, 1, "פתיתים אפויים 500 גר' אסם", "C:\\Users\\Evyatar\\Desktop\\2.jpg", ProductsCategory.Food),
                 new PurchasedProduct(new DateTime(2020, 11, 2), 5, 2, 2, "לחם אחיד פרוס אנג'ל", "C:\\Users\\Evyatar\\Desktop\\1.jpg", ProductsCategory.Food),
                 new PurchasedProduct(new DateTime(2020, 12, 2), 20, 3, 3, "חלב 3% שומן תנובה", "C:\\Users\\Evyatar\\Desktop\\3.jpg", ProductsCategory.Health),
                 new PurchasedProduct(new DateTime(2020, 11, 2), 5, 2, 2, "גבינת קוטג' 5% תנובה", "https://www.tnuva.co.il/uploads/f_5d0f283c02962_1561274428.png", ProductsCategory.Food),
-                new PurchasedProduct(new DateTime(2020, 3, 5), 20, 3, 3, "ממרח נוטלה 350 גר'", "https://www.farfel.co.il/wp-content/uploads/2018/01/127.jpg", ProductsCategory.Fashion),
+                new PurchasedProduct(new DateTime(2021, 3, 8), 20, 3, 3, "ממרח נוטלה 350 גר'", "https://www.farfel.co.il/wp-content/uploads/2018/01/127.jpg", ProductsCategory.Fashion),
                 new PurchasedProduct(new DateTime(2020, 11, 2), 5, 2, 2, "בושם לגברים Lacoste Essential 75 מ\"ל ", "https://superpharmstorage.blob.core.windows.net/hybris/products/desktop/medium/737052483214.jpg", ProductsCategory.Food),
                 new PurchasedProduct(new DateTime(2020, 12, 2), 20, 3, 3, "חולצת פולו ראלף לורן M", "C:\\Users\\Evyatar\\Desktop\\ral.jpg", ProductsCategory.Fashion),
                 new PurchasedProduct(new DateTime(2020, 12, 2), 3793, 3, 3, "טלוויזיה Samsung QE65Q60T 4K ‏65 ‏אינטש", "https://img.zap.co.il/pics/3/2/5/1/56591523c.gif", ProductsCategory.Health),
                 new PurchasedProduct(new DateTime(2020, 12, 2), 7, 4, 4, "מחשב אינטל NUC", "https://www.ivory.co.il/files/catalog/org/1566199487C87HL.jpg", ProductsCategory.Electricity)
+
             };
 
         }
@@ -74,7 +76,9 @@ namespace EcoBuy.ViewModels
         }
         public void FilterByDay()
         {
-            PurchasedProducts = (ObservableCollection<PurchasedProduct>)PurchasedProducts.Where(prod => prod.PurchaseDate == DateTime.Now);
+            var items = this.PurchasedProducts.Where(prod => prod.PurchaseDate == DateTime.Now.Date).ToList();
+            PurchasedProducts = new ObservableCollection<PurchasedProduct>(items);
+
         }
     }
 }
