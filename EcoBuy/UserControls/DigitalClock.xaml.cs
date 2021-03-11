8cfa9e4d54bd40e74e59d896f1dfaced65298412
Evyatar Revivo <50528768+erevivo@users.noreply.github.com>
2021-03-11 16:29:36 +0200

    d

--- EcoBuy/UserControls/DigitalClock.xaml.cs
+++ EcoBuy/UserControls/DigitalClock.xaml.cs
@@ -1,6 +1,9 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
+using System.Net.Http;
 using System.Text;
 using System.Windows;
 using System.Windows.Controls;
@@ -20,9 +23,15 @@ namespace EcoBuy.UserControls
     public partial class DigitalClock : UserControl
     {
         System.Windows.Threading.DispatcherTimer Timer = new System.Windows.Threading.DispatcherTimer();
+
+
+
+
         public DigitalClock()
         {
+
             InitializeComponent();
+            Temp();
             Timer.Tick += new EventHandler(Timer_Click);
             Timer.Interval = new TimeSpan(0, 0, 1);
             Timer.Start();
@@ -35,7 +44,22 @@ namespace EcoBuy.UserControls
             if (d.Minute < 10 && d.Minute >= 0)
                 label1.Content = d.Hour + " : " + "0" + d.Minute;
             else
-            label1.Content = d.Hour + " : " + d.Minute;
+                label1.Content = d.Hour + " : " + d.Minute;
+            hedate.Content = d.Date.ToString("dd/MM/yyyy");
+        }
+        private async void Temp()
+        {
+            HttpClient client = new HttpClient();
+            client.BaseAddress = new Uri("http://api.openweathermap.org");
+            var response = await client.GetAsync($"/data/2.5/weather?q=Ashdod&appid=90f9786ae346b4c7d7d14175a35e4118");
+
+            // This line gives me error
+            var stringResult = await response.Content.ReadAsStringAsync();
+
+            var obj = JsonConvert.DeserializeObject<dynamic>(stringResult);
+            var tmpDegreesF = Math.Round(((float)obj.main.temp * 9 / 5 - 459.67), 2);
+            var tmpDegreesC = ((tmpDegreesF - 32) * (0.55555555556));
+            temp.Content = tmpDegreesC.ToString();
         }
 
     }
